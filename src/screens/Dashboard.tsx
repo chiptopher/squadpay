e7c3d996ef557de30d33bc3839372cd32d9e83ea
chiptopher <chrisboyerdev@gmail.com>
2019-08-04 15:17:10 -0400

    Move to using scss file for styles

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -6,6 +6,8 @@ import {MemberListElement} from "../components/MemberListElement";
 import {money} from "../util/money";
 import logo from "../logo.png"
 
+import "./Dashboard.scss";
+
 export interface Props {
 
 }
@@ -39,25 +41,25 @@ export class Dashboard extends React.Component<Props, State> {
         this.setState({width: window.innerWidth});
     };
 
-    generateContainerStyle() {
+    generateContainerStyle(): string {
         const isMobile = this.state.width <= 500;
-        let finalStyle = isMobile ? {...styles.squadContainer, ...{width: "100%"}} : styles.squadContainer;
-        return finalStyle
+        let finalStyle = isMobile ? 'mobileSquadContainer' : 'squadContainer';
+        return finalStyle;
     }
 
     render() {
-        return <div style={styles.container}>
-            <div style={styles.title}>
-                <img style={styles.logo} src={logo}/>
-                <span style={styles.titleSquad}>squad</span>
-                <span style={styles.titlePay}>pay</span>
+        return <div className={"container"}>
+            <div className={"title"}>
+                <img className={"logo"} src={logo}/>
+                <span className={"titleSquad"}>squad</span>
+                <span className={"titlePay"}>pay</span>
             </div>
-            <div style={this.generateContainerStyle()}>
-                <div style={styles.costOfTrip}>
-                    <span style={styles.costOfTripText}>Trip Cost:</span>
-                    <span style={styles.costOfTripMoney}>{money(this.state.squad.costOfTrip())}</span>
+            <div className={this.generateContainerStyle()}>
+                <div className={"costOfTrip"}>
+                    <span className={"costOfTripText"}>Trip Cost:</span>
+                    <span className={"costOfTripMoney"}>{money(this.state.squad.costOfTrip())}</span>
                 </div>
-                <div style={styles.addMember}>
+                <div className={"addMember"}>
                     <AddMember onMemberSubmit={(member) => {
                         const squad = this.state.squad;
                         squad.addSquadMember(member);
@@ -66,10 +68,10 @@ export class Dashboard extends React.Component<Props, State> {
                         })
                     }}/>
                 </div>
-                <div style={styles.membersContainer}>
+                <div className={"membersContainer"}>
                     {
                         this.state.squad.squadMembers.map((member) => {
-                            return <div key={member.name} style={styles.member}>
+                            return <div key={member.name} className={"member"}>
                                 <MemberListElement member={member} squad={this.state.squad}/>
                             </div>
                         })
