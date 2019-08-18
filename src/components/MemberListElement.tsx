1a30713f9352b62cddb2135cc919212802f56ca6
chiptopher <chrisboyerdev@gmail.com>
2019-08-18 08:13:19 -0400

    Remove unused code from member list element

--- src/components/MemberListElement.tsx
+++ src/components/MemberListElement.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import {Member} from "../models/Member";
 import {Squad} from "../models/Squad";
-import {money} from "../util/money";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faAngleDown, faAngleRight} from "@fortawesome/free-solid-svg-icons";
 
 import "./MemberListElement.scss";
 
@@ -12,39 +9,14 @@ interface Props {
     squad: Squad
 }
 
-interface State {
-    toggled: boolean
-}
-
-export class MemberListElement extends React.Component<Props, State> {
-    constructor(props: Readonly<Props>) {
-        super(props);
-        this.state = {
-            toggled: false
-        };
-        this.otherSquadMembers = this.otherSquadMembers.bind(this);
-    }
-
-    render() {
-        return <div className={"MemberListElement"}>
-            <div id={this.props.member.name.replace(/\s+/g, "-")}
-                 onClick={() => {
-                     this.setState({
-                         toggled: !this.state.toggled
-                     })
-                 }}>
-                <div className={"memberHeader"}>
-                    <div>
-                        {this.props.member.name}
-                    </div>
+export function MemberListElement(props: Props) {
+    return <div className={"MemberListElement"}>
+        <div id={props.member.name.replace(/\s+/g, "-")}>
+            <div className={"memberHeader"}>
+                <div>
+                    {props.member.name}
                 </div>
             </div>
         </div>
-    }
-
-    otherSquadMembers(): Member[] {
-        return this.props.squad.squadMembers.filter((member: Member) => {
-            return member !== this.props.member;
-        });
-    }
+    </div>;
 }
